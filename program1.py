87cdcd0ffccedd3dbd97b56e5c92883a3fc6b24e
yashvant <yashvanttalawar6@gmail.com>
2025-11-28 20:20:33 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,18 +1,13 @@
 import sys
 
-if len(sys.argv) == 4:
-    script_name = sys.argv[0]
-    principal = sys.argv[1]
-    rate = sys.argv[2]
-    time = sys.argv[3]
-else:
-    principal = 1000.0
-    rate = 5.0
-    time = 10
+if len(sys.argv) != 4:
+    print("Usage: python simple_interest.py <principal> <rate> <time>")
+    sys.exit()
+
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
 
 simple_interest = (principal * rate * time) / 100
 
-print("Principal Amount is:", principal)
-print("Rate of Interest is:", rate)
-print("Time Period is:", time)
-print("Simple Interest is:", simple_interest)
+print("Simple Interest:", simple_interest)
